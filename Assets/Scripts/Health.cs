0af48dbbd18ebd59c2ff48cb76d4e9ef0a659493
Martin Vaskoski <martinvaskoski@gmail.com>
2023-06-25 21:05:10 +0200

    Added explosion effect when enemy/player hit

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class Health : MonoBehaviour {
     [SerializeField] private int health = 50;
+    [SerializeField] private ParticleSystem _hitEffect; 
 
     private void OnTriggerEnter2D(Collider2D other) {
         DamageDealer dmgDealer = other.GetComponent<DamageDealer>();
 
         if (dmgDealer != null) {
             TakeDamage(dmgDealer.GetDamage());
+            PlayHitEffect();
             dmgDealer.Hit();
         }
     }
@@ -19,6 +21,13 @@ public class Health : MonoBehaviour {
         health -= damageAmount;
         if (health <= 0) {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect() {
+        if (_hitEffect !=  null) {
+            ParticleSystem instance = Instantiate(_hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
